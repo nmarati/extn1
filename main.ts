db5e517b1072c91eceb2e2b5e4fdd8eb3ad7d10b
nmarati <narayanan.marati@gmail.com>
2021-03-27 23:33:49 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -21,7 +21,7 @@ namespace custom {
      * @param value describe value here, eg: 5
      */
     //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    export function test(value: number): number {
+        return value <= 1 ? value : test(value -1) + test(value - 2);
     }
 }
