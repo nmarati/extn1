4ce074c80380695897a414b6031a99be71e11d59
nmarati <narayanan.marati@gmail.com>
2021-03-27 23:34:56 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -18,7 +18,7 @@ namespace custom {
 
     /**
      * TODO: describe your function here
-     * @param value describe value here, eg: 5
+     * @param value describe value here, eg: 15
      */
     //% block
     export function test(value: number): number {
