d67109ea1628220068f03c087e3da0820f0fafbf
nmarati <narayanan.marati@gmail.com>
2021-03-27 23:31:47 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,13 @@
-
-/**
- * Use this file to define custom functions and blocks.
- * Read more at https://makecode.microbit.org/blocks/custom
- */
-
 enum MyEnum {
     //% block="one"
     One,
     //% block="two"
     Two
 }
-
-/**
- * Custom blocks
- */
-//% weight=100 color=#0fbc11 icon=""
 namespace custom {
     /**
      * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
+     * @param n describe parameter here, eg: 6
      * @param s describe parameter here, eg: "Hello"
      * @param e describe parameter here
      */
